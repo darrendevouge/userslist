a97ede2251e2872930a78c031802e733a6d4368b
Darren De Vouge <darrn.dv@gmail.com>
2023-10-12 16:52:22 -0400

    How refs can you used in place of state with user inputs

--- src/components/AddUser/AddUser.js
+++ src/components/AddUser/AddUser.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import Card from '../Card/Card';
 import Button from '../Button/Button';
 import classes from './AddUser.module.css';
@@ -6,13 +6,24 @@ import ErrorModal from '../ErrorModal/ErrorModal';
 
 const AddUser = (props) => {
 
-    const [enteredUserName,  setEnteredUserName] = useState('');
-    const [enteredAge, setEnteredAge] = useState('');
+    /* Using refs instead of state to read NOT manipulate the actual DOM when prudent (not used often) */
+    /* Form inputs would be an example. */
+    /* These inputs are also called "Uncontrolled Components" */
+
+    //const [enteredUserName,  setEnteredUserName] = useState('');
+    //const [enteredAge, setEnteredAge] = useState('');
     const [error, setError] = useState();
+
+    const nameInputRef = useRef();
+    const ageInputRef = useRef();
 
     const addUserHandler = (e) => {
         e.preventDefault();
-        if(enteredUserName.trim().length === 0 || enteredAge.trim().length === 0){
+
+        const enteredName = nameInputRef.current.value; //use this in place of state which would log every key stroke.
+        const enteredAge = ageInputRef.current.value;
+
+        if(enteredName.trim().length === 0 || enteredAge.trim().length === 0){
             setError({
                 title: 'Invalid Input',
                 message: 'Please enter a valid name and age (non-empty values).'
@@ -26,11 +37,14 @@ const AddUser = (props) => {
             })
             return;
         }
-        props.onAddUser(enteredUserName, enteredAge);
-        setEnteredUserName('');
-        setEnteredAge('');
+        props.onAddUser(enteredName, enteredAge);
+        //setEnteredUserName('');
+        //setEnteredAge('');
+        nameInputRef.current.value = '';
+        ageInputRef.current.value = '';
     }
 
+    /* NO LONGER NEEDED with refs
     const userNameChangeHandler = (e) => {
         setEnteredUserName(e.target.value);
     }
@@ -38,6 +52,8 @@ const AddUser = (props) => {
     const ageChangeHandler = (e) => {
         setEnteredAge(e.target.value);
     }
+
+    */
 
     const errorHandler = () => {
         setError(null);
@@ -50,9 +66,20 @@ const AddUser = (props) => {
             <Card className={classes.input}>
                 <form onSubmit={addUserHandler} >
                     <label htmlFor="name">Name</label>
-                    <input id="username" type="text" value={enteredUserName}  onChange={userNameChangeHandler} />
+                    <input  id="username" 
+                            type="text" 
+                            //value={enteredUserName}
+                            //onChange={userNameChangeHandler}
+                            ref={nameInputRef}
+                            
+                    />
                     <label htmlFor="age">Age (years)</label>
-                    <input id="age" type="number"  value={enteredAge}  onChange={ageChangeHandler}/>
+                    <input id="age"
+                            type="number"
+                            //value={enteredAge}
+                            //onChange={ageChangeHandler}
+                            ref={ageInputRef}
+                     />
                     <Button className={classes.button} type="submit">Add User</Button>
                 </form>
             </Card>
